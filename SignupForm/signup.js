591e69d8193993e4563d6a13783402ec2554b806
RK <kadufluffy@gmail.com>
2024-06-27 23:04:34 +0500

    Add files via upload

---
+++ SignupForm/signup.js
@@ -0,0 +1,51 @@
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone-number");
+const birthDate = document.getElementById("birth-date");
+const password = document.getElementById("password");
+const passwordConfirm = document.getElementById("confirm-password");
+const passwordConfirmError = document.getElementById("confirm-password-error");
+const submitButton = document.getElementById("submit");
+const form = document.getElementById("form");
+
+function validatePasswordConfirm() {
+  if (password.value === passwordConfirm.value) {
+    passwordConfirmError.innerHTML = null;
+    passwordConfirm.style.borderColor = "green";
+    password.style.borderColor = "green";
+    return true;
+  } else {
+    passwordConfirmError.innerHTML = "*Passwords do not match";
+    passwordConfirm.style.borderColor = "red";
+    password.style.borderColor = "red";
+    return false;
+  }
+}
+function resetStyles() {
+  document.getElementById("password").style.borderColor = "";
+  document.getElementById("confirm-password").style.borderColor = "";
+}
+
+function validateForm() {
+  const fieldsFilled =
+    firstName.value &&
+    lastName.value &&
+    email.value &&
+    phone.value &&
+    birthDate.value;
+  return validatePasswordConfirm() && fieldsFilled;
+}
+// Add event listener to the confirm password field to validate on input
+passwordConfirm.addEventListener("input", validatePasswordConfirm);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (validateForm()) {
+    alert("Form submitted successfully");
+    form.reset();
+    resetStyles();
+  } else {
+    alert("Form is invalid. Please fix highlighted fields.");
+  }
+});
