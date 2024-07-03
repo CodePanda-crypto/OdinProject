1ad29cafc5828a005ea05c32554bae3227773c35
CodePanda-crypto <kadufluffy@gmail.com>
2024-07-03 14:48:43 +0500

    webpack tut

---
+++ webpack/src/index.js
@@ -0,0 +1,5 @@
+// another JS file
+// Note: You can only use single quotes (') or double quotes (") for import statements 
+import { functionOne } from './fun';
+
+functionOne(); // this should work as expected!
